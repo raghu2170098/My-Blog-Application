a570082545acbc091e3f62057edfa093edb276cd
raj75045 <2170098@kiit.ac.in>
2024-01-04 17:36:24 +0530

    127:Create save Post Feature

---
+++ src/main/java/com/myblog3/Myblog3Application.java
@@ -0,0 +1,13 @@
+package com.myblog3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Myblog3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Myblog3Application.class, args);
+	}
+
+}
